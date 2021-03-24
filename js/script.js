49ae34783b100d24fbef804d66ee5e6f58331fad
GianlucaBar <gianlucabarce@gmail.com>
2021-03-24 18:17:44 +0100

    Correzione

--- js/script.js
+++ js/script.js
@@ -11,33 +11,37 @@ var posizione = -1;
 
 // con for 
 
-  for ( var i=0; i < listaUtenti.length; i++){
-     var thisElement = listaUtenti[i];
-      if( thisElement == cognomeUtente ){
-          posizione = i + 1;
-      }
-      document.getElementById('listaCognomi').innerHTML += '<li>' + thisElement + '</li>';
-  }
+//   for ( var i=0; i < listaUtenti.length; i++){
+//      var thisElement = listaUtenti[i];
+//       if( thisElement == cognomeUtente ){
+//           posizione = i + 1;
+//       }
+//       document.getElementById('listaCognomi').innerHTML += '<li>' + thisElement + '</li>';
+//   }
 
 // con while 
 
-//  var i = 0;
+ var i = 0;
 
-//  while( i < listaUtenti.length ){
+  while( i < listaUtenti.length ){
 
-//     var thisElement = listaUtenti[i]; 
+     var thisElement = listaUtenti[i]; 
 
-//      if ( thisElement == cognomeUtente ){
+      if ( thisElement == cognomeUtente ){
 
-//          posizione = i + 1;
-//          i = listaUtenti.length;
-//      }
+          posizione = i + 1;
 
-//     document.getElementById('listaCognomi').innerHTML += '<li>' + thisElement + '</li>'; 
-//     i++;
-//  }
+      }
 
-document.getElementById('posizione-in-lista').innerHTML = 'Ti trovi alla posizione ' + posizione + ' in lista';
+     document.getElementById('listaCognomi').innerHTML += '<li>' + thisElement + '</li>'; 
+     i++;
+  }
+if( posizione == -1){
+    document.getElementById('posizione-in-lista').innerHTML = 'Cognome non in lista';
+} else{
+    document.getElementById('posizione-in-lista').innerHTML = 'Ti trovi alla posizione ' + posizione + ' in lista';
+}
+
 
 console.log(listaUtenti);
 
